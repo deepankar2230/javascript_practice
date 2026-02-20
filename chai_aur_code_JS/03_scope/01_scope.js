deced6cfd756266ac8ae1409b6b9089e00c30b3d
deepankar2230 <deepankarmali2001@gmail.com>
2026-02-20 14:13:31 +0000

    hello added

--- chai_aur_code_JS/03_scope/01_scope.js
+++ chai_aur_code_JS/03_scope/01_scope.js
@@ -1,9 +1,11 @@
-if(true){
-    const a = 'hello'
-    if (true){
-        const b = 'llol'
-        console.log(a)
-    }
-    // console.log(b)
-}
-// console.log(a)
+// if(true){
+//     const a = 'hello'
+//     if (true){
+//         const b = 'llol'
+//         console.log(a)
+//     }
+//     // console.log(b)
+// }
+// // console.log(a)
+
+console.log('hello');
